87dcad055362d08f5933171b25a6bd57d7e1614e
prasadbudda <bprasad.cse2011@iaitc.in>
2015-11-16 10:20:07 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,16 @@
 console.log('This would be the main JS file.');
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '907050789387318',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
