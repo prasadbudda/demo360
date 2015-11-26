5556c314268c1d49df4246572dc4a7812b95025d
prasadbudda <bprasad.cse2011@iaitc.in>
2015-11-26 13:15:03 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,11 @@
 console.log('This would be the main JS file.');
 
-<script>
+
 // Only works after `FB.init` is called
 function myFacebookLogin() {
   FB.login(function(){}, {scope: 'publish_actions'});
 }
-</script>
+
 
  // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
