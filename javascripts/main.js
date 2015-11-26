650664cd192270e561ec22729384bce882485921
prasadbudda <bprasad.cse2011@iaitc.in>
2015-11-26 14:11:32 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -78,11 +78,11 @@ function myFacebookLogin() {
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
+    function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
-    });
-  }
+          window.location = "http://prasadbudda.github.io/demo360/logged.html";
+      });
